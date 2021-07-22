79319d9460d5e5fdc80e1aa28b599043c3508913
zak aden <s.aden159@gmail.com>
2021-07-22 20:35:09 +0100

    styling added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import './App.css';
 import Navbar from './Components/Navbar';
+import Home from './Components/Home';
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
       <div class='content'>
-      <h3>hello</h3>
+      <Home />
       </div>
     </div>
   );
