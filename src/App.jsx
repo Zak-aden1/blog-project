7b04b4148aa06a094d48e4c08a1890b9017471a3
zak aden <s.aden159@gmail.com>
2021-07-22 20:24:24 +0100

    home component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css';
+import Navbar from './Components/Navbar';
 
 const App = () => {
   return (
     <div className="App">
+      <Navbar />
+      <div class='content'>
       <h3>hello</h3>
+      </div>
     </div>
   );
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import styles from './Navbar.module.scss'
 
 const Navbar = () => {
     return (
-        <div>
-            
-        </div>
+
+            <nav className={styles.navbar}>
+                <h1>The Dojo Blog</h1>
+                <div className={styles.links}>
+                    <a href="/">Home</a>
+                    <a href="/create">New Blog</a>
+                </div>
+            </nav>
+
     )
 }
 
