3374b9962d82aa7db349635aecf313115a886766
zak aden <s.aden159@gmail.com>
2021-07-22 21:09:55 +0100

    styling added to Home component

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import styles from './Home.module.scss'
+import { useState } from 'react'
 
 const Home = () => {
+    const [blogs, setBlogs] =useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ])
     return (
-        <div>
-            <h2>Homepage</h2>
+        <div className={styles.home}>
+            {blogs.map((blog) => (
+                <div className={styles.blogPreview} key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written By {blog.author}</p>
+                </div>
+            ))}
         </div>
     )
 }
