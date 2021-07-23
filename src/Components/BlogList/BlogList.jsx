632050a7a7b6abd12a9427a306ca13daba5655d3
zak aden <s.aden159@gmail.com>
2021-07-23 19:50:56 +0100

    delete functionality added

--- src/Components/BlogList/BlogList.jsx
+++ src/Components/BlogList/BlogList.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import styles from './BlogList.module.scss'
 
-const BlogList = () => {
+const BlogList = ({blogs, handleDelete}) => {
     return (
         <div>
-            
+            <h2>All Blogs!</h2>
+             {blogs.map((blog) => (
+                <div className={styles.blogPreview} key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written By {blog.author}</p>
+                    <button onClick={() => {handleDelete(blog.id)}}>Delete</button>
+                </div>
+            ))}
         </div>
     )
 }
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './Home.module.scss'
 import { useState } from 'react'
+import BlogList from '../BlogList'
 
 const Home = () => {
     const [blogs, setBlogs] =useState([
@@ -8,14 +9,15 @@ const Home = () => {
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ])
+
+  const handleDelete = (id) => {
+    const newBlog = blogs.filter((blog) => blog.id != id)
+    setBlogs(newBlog)
+  }
     return (
         <div className={styles.home}>
-            {blogs.map((blog) => (
-                <div className={styles.blogPreview} key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>Written By {blog.author}</p>
-                </div>
-            ))}
+            <BlogList blogs={blogs} handleDelete={handleDelete}/>
+            <BlogList blogs={blogs.filter((blog) => blog.author.includes('yoshi'))} />
         </div>
     )
 }
