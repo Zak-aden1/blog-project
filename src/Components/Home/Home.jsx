a306225afce92d79171629f01b02c1b7475fa90e
zak aden <s.aden159@gmail.com>
2021-07-23 20:32:41 +0100

    json server added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,18 +2,30 @@ import React from 'react'
 import styles from './Home.module.scss'
 import { useState } from 'react'
 import BlogList from '../BlogList'
+import { useEffect } from 'react'
 
 const Home = () => {
-    const [blogs, setBlogs] =useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ])
+    const [blogs, setBlogs] =useState(null)
+
 
   const handleDelete = (id) => {
     const newBlog = blogs.filter((blog) => blog.id != id)
     setBlogs(newBlog)
   }
+
+  useEffect((e) => {
+      fetch('http://localhost:8000/blogs')
+      .then(res => { 
+          return res.json()
+        })
+      .then(data => {
+          console.log(data)
+          setBlogs(data)
+        })
+      .catch(err => {
+          alert('error')
+        })
+  },[])
     return (
         <div className={styles.home}>
             <BlogList blogs={blogs} handleDelete={handleDelete}/>
